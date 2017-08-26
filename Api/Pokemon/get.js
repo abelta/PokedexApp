0de1fff2b01b1d09812eca21691b40f9e9a601be
Abel <abel.tamayo@gmail.com>
2017-08-26 13:32:32 +0200

    Fetch helpers added. Json call working.

--- Api/Pokemon/get.js
+++ Api/Pokemon/get.js
@@ -1,3 +1,9 @@
-const get = ({ id }) => fetch(`http://pokeapi.co/api/v2/pokemon/${id}`);
+import { checkStatus, parseJson } from '../../Helpers/fetch';
+
+const get = ({ id }) => (
+  fetch(`http://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(checkStatus)
+    .then(parseJson)
+);
 
 export default get;

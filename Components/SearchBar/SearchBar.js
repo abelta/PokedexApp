0af3d4d711f0926d78035f96bb79c3d2c4b52d3f
Abel <abel.tamayo@gmail.com>
2017-09-10 00:15:30 +0200

    Working on SearchBar functionality.

--- Components/SearchBar/SearchBar.js
+++ Components/SearchBar/SearchBar.js
@@ -5,25 +5,52 @@ import { Back as BackIcon, Clear as ClearIcon } from '../../Components/Icons';
 import styles from './styles';
 
 class SearchBar extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+    this.onChangeText = this.onChangeText.bind(this);
+    this.onClear = this.onClear.bind(this);
+    this.onSubmitEditing = this.onSubmitEditing.bind(this);
+  }
+
+  onChangeText(value) {
+    this.setState({ value });
+  }
+
+  onClear() {
+    const value = '';
+    this.setState({ value });
+  }
+
+  onSubmitEditing({event, value, text}) {
+    console.log('ON SUBMIT');
+    // console.log(event);
+    // console.log(value);
+    // console.log(text);
+  }
+
   render() {
+    const { value } = this.state;
     return (
       <View style={styles.searchBar}>
-        <TouchableLink style={styles.back} to="/" ><BackIcon /></TouchableLink>
+        <TouchableLink style={styles.back} to="/" >
+          <BackIcon />
+        </TouchableLink>
         <TextInput
           autoCorrect={false}
           autoFocus
           multiline={false}
+          onChangeText={this.onChangeText}
+          onSubmitEditing={this.onSubmitEditing}
           placeholder="Find Pokèmon by name"
           placeholderTextColor="lightgrey"
           returnKeyType="search"
           selectTextOnFocus
           style={styles.input}
           underlineColorAndroid="transparent"
+          value={value}
         />
-        <TouchableButton
-          onPress={() => { console.warn('PRESS') }}
-          style={styles.clear}
-        >
+        <TouchableButton onPress={this.onClear} style={styles.clear}>
           <ClearIcon />
         </TouchableButton>
       </View>

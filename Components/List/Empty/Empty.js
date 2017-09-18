dcb9aea39c435bd22926c9bbaba96b087e8a3f00
Abel <abel.tamayo@gmail.com>
2017-09-18 16:10:53 +0200

    Styled empty list.

--- Components/List/Empty/Empty.js
+++ Components/List/Empty/Empty.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Text, View } from 'react-native';
+import styles from './styles';
 
-const Empty = () => (
-  <View>
-    <Text>...loading data</Text>
+const Empty = ({ style }) => (
+  <View style={[styles.empty, style]}>
+    <Text style={styles.text}>...loading data</Text>
   </View>
 );
+
+Empty.propTypes = {
+  style: PropTypes.number, // eslint-disable-line react/require-default-props
+};
 
 export default Empty;
---
+++ Components/List/Empty/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  empty: {
+    borderColor: 'lightgrey',
+    borderWidth: StyleSheet.hairlineWidth,
+    height: 35,
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
+
+export default styles;
